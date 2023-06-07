5f8414ddd9c472ac94466dc10132091639c32a2a
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-06-07 14:43:51 +0530

    Async JS added

---
+++ Async_JS/Callaback/app.js
@@ -0,0 +1,10 @@
+setTimeout(()=>
+{
+    document.body.style.backgroundColor='red';
+    setTimeout(()=>{
+        document.body.style.backgroundColor='yellow';
+        setTimeout(()=>{
+            document.body.style.backgroundColor='green';
+        },1000)
+    },1000)
+},1000)
