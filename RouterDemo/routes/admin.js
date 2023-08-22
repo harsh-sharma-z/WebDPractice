96d352baef24cdc098bd216e64a2ea11f27e3df7
harsh-sharma-z <harsh.ramakant.sharma@gmail.com>
2023-08-22 11:57:33 +0530

    Basic router

---
+++ RouterDemo/routes/admin.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.use((req , res , next) =>{
+    if (req.query.isAdmin){
+        next();
+    }
+    res.send("Sorrt not an admin")
+})
+
+router.get('/topsecret' , (req , res) =>{
+    res.send('This is top secret')
+})
+
+router.get('/deleteeverything' , (req , res) =>{
+    res.send('Okay deleted it all')
+})
+
+
+module.exports = router;
